3df540a774e4b73c8b9129c34f03795672985eb7
clemstruck <69113110+clemstruck@users.noreply.github.com>
2025-02-13 13:03:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -204,7 +204,7 @@ def main():
             records.append({
                 "ext2_ticker": ticker,
                 "timestamp_created_utc": now_utc,
-                "date": row.get("date"),
+                "update_date": row.get("date"),
                 "open": row.get("open"),
                 "high": row.get("high"),
                 "low": row.get("low"),
@@ -236,7 +236,7 @@ def main():
                         chunk_df[[
                             "ext2_ticker",
                             "timestamp_created_utc",
-                            "date",
+                            "update_date",
                             "open",
                             "high",
                             "low",
